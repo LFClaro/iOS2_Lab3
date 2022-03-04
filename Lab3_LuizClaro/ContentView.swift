af44dfc5a7b9fa3b0edf1b3e3b2f3036fc77d82c
Luiz Claro <lfclaro@gmail.com>
2022-03-04 12:46:56 -0500

    Initial Commit

---
+++ Lab3_LuizClaro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab3_LuizClaro
+//
+//  Created by Luiz Fernando Reis on 2022-03-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab3_LuizClaro/Lab3_LuizClaroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab3_LuizClaroApp.swift
+//  Lab3_LuizClaro
+//
+//  Created by Luiz Fernando Reis on 2022-03-04.
+//
+
+import SwiftUI
+
+@main
+struct Lab3_LuizClaroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
