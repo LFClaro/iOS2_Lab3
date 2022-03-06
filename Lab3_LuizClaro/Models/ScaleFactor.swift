b9a6cef0d4831f691aeb2f95f6b4509a17860f4d
Luiz Claro <lfclaro@gmail.com>
2022-03-05 23:54:47 -0500

    Fist commit

---
+++ Lab3_LuizClaro/Models/ScaleFactor.swift
@@ -0,0 +1,13 @@
+//
+//  ScaleFactor.swift
+//  Lab3_LuizClaro
+//
+//  Created by Luiz Fernando Reis on 2022-03-05.
+//
+
+import SwiftUI
+
+struct ScaleFactor {
+    var w = UIScreen.main.bounds.size.width
+    var h = UIScreen.main.bounds.size.height
+}
